3af74a43a779ff8a1ed1781a87f35dc37f173029
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-09-10 20:56:18 -0300

    added file highlight string

---
+++ HighLightString.js
@@ -0,0 +1,10 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dojo/query',
+    'dojo/dom-construct',
+    'dojo/NodeList-manipulate'
+], function( declare , lang, query , domConstruct ){        
+
+
+});
