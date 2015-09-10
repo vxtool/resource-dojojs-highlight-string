3193641da5883f07c070b14d3b0b59da1076ffaa
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-09-10 20:57:43 -0300

    added parameters and constructor

--- HighLightString.js
+++ HighLightString.js
@@ -6,5 +6,18 @@ define([
     'dojo/NodeList-manipulate'
 ], function( declare , lang, query , domConstruct ){        
 
+    return declare(null, {
+
+        cssClass: 'highlight',
+
+        searchNode: null,
+
+        cache: null,
+
+        constructor: function( opcoes ){
+            lang.mixin( this, opcoes );
+
+         }
+    });
 
 });
