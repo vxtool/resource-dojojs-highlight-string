ccb8d9ed969c0d81f1e63c99df66692bbc0af829
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-09-12 21:00:41 -0300

    doc Highlight String

--- HighLightString.js
+++ HighLightString.js
@@ -5,19 +5,69 @@ define([
     'dojo/dom-construct',
     'dojo/NodeList-manipulate'
 ], function( declare , lang, query , domConstruct ){        
-
+        /**
+     * @class HighLightString
+     * Destaca a string passada como parametro
+     *
+     * @param {Object} dom Elemento que será validado
+     *
+     * Para utilizar a validação em formato de função é necessário que a
+     * função atenda alguns pre-requisitos.
+     *
+     *     @example Metodo de validação por metodo
+     *     var substituir = new HighLightString({
+     *          cssClass: '[classe_do_elemento]',
+     *          searchNode: dom.byId('[id_do_elemento]')
+     *     });
+     *
+     *     new validaCampo(query('input#nome'), new validaNome() );
+     */
     return declare(null, {
-
+        /**
+         * CLASS css
+         *
+         * @property cssClass
+         * @type {String}
+        **/
         cssClass: 'highlight',
-
+        /**
+         * The DOM element
+         *
+         * @property searchNode
+         * @type {HTMLElement}
+         * @example 
+         *     // usando o id
+         *     dom.byId('nome_do_id');
+        **/
         searchNode: null,
-
+        /**
+         * The DOM element
+         *
+         * @property cache
+         * @static
+         * @type {HTMLElement}
+        **/
         cache: null,
-
+        /**
+         * constructor
+         *
+         * @method constructor
+         * @param cssClass
+         * @param searchNode
+         *
+        **/
         constructor: function( opcoes ){
             lang.mixin( this, opcoes );
 
          },
+        /**
+         * Método para replicar o html
+         *
+         * @param searchText
+         * @param replacement
+         * @param searchNode
+         * @chainable
+        **/
         replaceHtml: function( searchText, replacement, searchNode ){
             if (!searchText || typeof replacement === 'undefined') return;
 
@@ -57,6 +107,13 @@ define([
                 parent.removeChild(currentNode);
             }
         },
+        /**
+         * Limpa o cache do DOM consultado
+         *
+         *      var a = new HighLightString();
+         *      a.clear();
+         *
+        **/
         clear: function(){
             if(this.cache){
                 domConstruct.place( this.cache , this.searchNode, "after");
@@ -66,6 +123,16 @@ define([
                 this.cache = null; 
             }
         },
+        /**
+         * Passa como parâmetro a string e aciona a funcao replaceHtml 
+         *
+         *      var a = new HighLightString();
+         *      a.cssHighlight( valor_de_busca );
+         *
+         * @param searchText      
+         *
+         * @return {String} <span class=[classe_elemento]>[string_da_busca]</span>
+        **/
         cssHighlight: function( searchText ){
             var me = this;
 
