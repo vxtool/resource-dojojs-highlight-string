273ce3315b2c50b96ef2bb903aaef00b04de07d5
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-09-13 21:01:24 -0300

    added script

---
+++ script.js
@@ -0,0 +1,12 @@
+require([
+  'dojo/dom',
+  'dojo/query',
+  'dojo/on',
+  'dojo/keys',
+  'dojo/dom-attr',
+  './HighLightString',
+  'dojo/NodeList-manipulate'
+], function( dom, query, on, keys, domAttr, HighLightString ){
+
+  
+});
