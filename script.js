5bd11ac9ffc5ec08f8ce4a7540504e1747135c08
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-09-13 21:02:44 -0300

    rules for highlight string

--- script.js
+++ script.js
@@ -8,5 +8,29 @@ require([
   'dojo/NodeList-manipulate'
 ], function( dom, query, on, keys, domAttr, HighLightString ){
 
+  var substituir = new HighLightString({
+    cssClass: 'resultado_caracteristicas',
+    searchNode: dom.byId('box_prod_info')
+  });
+
   
+  query('#botao_pesquisar_caracteristicas').on("click", function(){
+    substituir.clear();
+    
+    var valor_busca = domAttr.get( dom.byId("inp_text_caracteristica") , "value" );
+    
+    substituir.cssHighlight( valor_busca );
+  });
+
+  query('#inp_text_caracteristica').on("keypress", function(evt){
+    var charOrCode = evt.charCode || evt.keyCode;
+
+    if(keys.ENTER === charOrCode || keys.NUMPAD_ENTER === charOrCode){
+      evt.preventDefault();
+      substituir.clear();
+      var valor_busca = domAttr.get( dom.byId("inp_text_caracteristica") , "value" );
+
+      substituir.cssHighlight( valor_busca ); 
+    }
+  });
 });
