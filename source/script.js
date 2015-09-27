efbec1fe9c3c7951a353e8dc6f7fc7cead990830
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-09-26 23:20:44 -0300

    adjusting script

--- source/script.js
+++ source/script.js
@@ -10,25 +10,25 @@ require([
 
   var _replace = new HighLightString({
     // definindo a classe do elemento que envolverá a String
-    cssClass: 'resultado_caracteristicas',
+    cssClass: 'result',
     // DOM onde será buscado a String
-    searchNode: dom.byId('box_prod_info')
+    searchNode: dom.byId('box')
   });
 
-  
-  query('#botao_pesquisar_caracteristicas').on("click", function(){
+
+  query('#button-search').on("click", function(){
     // No evento de clique ele chama a função clear da classe HighLightString para limpar o cache e caso tenha feito alguma busca anteriormente
     _replace.clear();
-    
+
     // Pega o valor do input onde foi iserido a string que será buscada
-    var _searchValue = domAttr.get( dom.byId("inp_text_caracteristica") , "value" );
-    
-    // cria o elemento que destaca a String e faz a busca 
+    var _searchValue = domAttr.get( dom.byId("inp-search") , "value" );
+
+    // cria o elemento que destaca a String e faz a busca
     _replace.cssHighlight( _searchValue );
   });
 
   // Evento caso o usuário não clique no botão e aperte a tecla Enter
-  query('#inp_text_caracteristica').on("keypress", function(evt){
+  query('#inp-search').on("keypress", function(evt){
     // Pega o code da tecla pressionada
     var charOrCode = evt.charCode || evt.keyCode;
 
@@ -37,10 +37,10 @@ require([
       // No evento de clique ele chama a função clear da classe HighLightString para limpar o cache e caso tenha feito alguma busca anteriormente
       _replace.clear();
       // Pega o valor do input onde foi iserido a string que será buscada
-      var _searchValue = domAttr.get( dom.byId("inp_text_caracteristica") , "value" );
+      var _searchValue = domAttr.get( dom.byId("inp-search") , "value" );
 
-      // cria o elemento que destaca a String e faz a busca 
-      _replace.cssHighlight( _searchValue ); 
+      // cria o elemento que destaca a String e faz a busca
+      _replace.cssHighlight( _searchValue );
     }
   });
 });
